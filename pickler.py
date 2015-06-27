a0b4e6931c9e2bae60a24ce1bb7f4e5b2570ed67
dcraven <dcravenus@gmail.com>
2015-06-27 09:11:17 -0600

    Pickler processes a directory automatically

--- pickler.py
+++ pickler.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import pickle
 from screen import Screen
 
@@ -5,28 +7,34 @@ WALL_CHAR = "w"
 PLAYER_CHAR = "x"
 EMPTY_CHAR = "."
 
+path = 'grids/'
+
 screen = Screen()
 
-grid = []
-with open('grids/gen5.txt', 'r') as f:
-    for line in f:
-        row = []
-        for char in line:
-            if char == WALL_CHAR:
-                row.append("WALL")
-            elif char == PLAYER_CHAR:
-                row.append("PLAYER")
-            elif char == EMPTY_CHAR:
-                row.append("EMPTY")
-            elif char == "\n" or char == " ":
-                pass
-            else:
-                print("Unrecognized character: " + char)
-        grid.append(row)
+for filename in glob.glob(os.path.join(path, '*.grid')):
+    grid = []
+    print("Pickling " + filename)
+    with open(filename, 'r') as f:
+        for line in f:
+            row = []
+            for char in line:
+                if char == WALL_CHAR:
+                    row.append("WALL")
+                elif char == PLAYER_CHAR:
+                    row.append("PLAYER")
+                elif char == EMPTY_CHAR:
+                    row.append("EMPTY")
+                elif char == "\n" or char == " ":
+                    pass
+                else:
+                    print("Unrecognized character: " + char)
+            grid.append(row)
 
 
-screen.height = len(grid)
-screen.width = len(grid[0])
-screen.grid = grid
+    screen.height = len(grid)
+    screen.width = len(grid[0])
+    screen.grid = grid
 
-pickle.dump(screen, open("grids/gen5.p", "wb"))
+    new_filename = os.path.splitext(filename)[0] + ".p"
+    print("Creating pickle " + new_filename)
+    pickle.dump(screen, open(new_filename, "wb"))
